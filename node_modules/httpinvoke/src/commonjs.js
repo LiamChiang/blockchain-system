8ee2be3789e6e471c33fa9cdf76349472658b01c
Louis Li <louisli486@gmail.com>
2018-08-15 22:28:24 +1000

    rm node folder

--- node_modules/httpinvoke/src/commonjs.js
+++
@@ -1,9 +0,0 @@
-if(typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.node !== 'undefined') {
-/* jshint unused:true */
-    var node;
-/* jshint unused:false */
-} else {
-/* jshint unused:true */
-    var browser;
-/* jshint unused:false */
-}
