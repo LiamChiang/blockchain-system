ceeb460c55d8df803e47f654e72eb9a6cc37d5f0
Louis Li <louisli@MacBook-Pro.local>
2018-08-15 18:02:42 +1000

    Create and Query account balance

--- index.js
+++ index.js
@@ -2,50 +2,54 @@ var openchain = require("openchain");
 var bitcore = require("bitcore-lib");
 var Mnemonic = require('bitcore-mnemonic');
 
-//Create new Passphrase = new wallet
-// var code = new Mnemonic();
-// console.log(code.toString());
+//TODO: Create a function to derive a walletId from private key.
 
-// // Load a private key from a seed
-// var privateKey = code.toHDPrivateKey();
-// var hdPrivateKey = new bitcore.HDPrivateKey(privateKey);
-// hdPrivateKey.network = bitcore.Networks.get("openchain");
-// var derivedKey = hdPrivateKey.derive(44, true).derive(64, true).derive(0, true).derive(0).derive(0);
+//Create new Passphrase / new user
+function createUser() {
+var code = new Mnemonic();
 
-// // Calculate the accounts corresponding to the private key
-// var walletPath = "/p2pkh/" + derivedKey + "/";
-
-// console.log("Wallet path: " + walletPath);
-
-
-//Existing Account
-// console.log("\nExisting");
-// var passphrase = "finish list orient tomorrow fit fine midnight satisfy cover antique creek tank";
-// var toMnenonic = new Mnemonic(passphrase);
-// var privateKey2 = toMnenonic.toHDPrivateKey();
-// console.log(privateKey2);
-
-// var address2 = hdPrivateKey.derive(44, true).derive(64, true).derive(0, true).derive(0).derive(0);
-// var tmp = address2.privateKey.toAddress().toString();
-
-// console.log(tmp);
-// // Calculate the accounts corresponding to the private key
-// var issuancePath2 = "/asset/p2pkh/" + address2 + "/";
-// var assetPath2 = issuancePath2;
-// var walletPath2 = "/p2pkh/" + address2 + "/";
-
-// console.log("Issuance path: " + issuancePath2);
-// console.log("Wallet path: " + walletPath2);
-
-var acc2 = "license saddle depart vintage nurse promote renew suggest steak already flush avocado";
-var acc2Mne = new Mnemonic(acc2);
-// var secondPrivateKey = acc2Mne.toHDPrivateKey();
-// console.log(secondPrivateKey.);
-
-var t = new Mnemonic();
-var makeKey = acc2Mne.toHDPrivateKey() //I think u missed toHDprivatekey
-var hdPrivateKey = new bitcore.HDPrivateKey(makeKey.toString());
+//Load a private key from a seed
+var privateKey = code.toHDPrivateKey();
+var hdPrivateKey = new bitcore.HDPrivateKey(privateKey.toString());
 hdPrivateKey.network = bitcore.Networks.get("openchain");
-derivedKey = hdPrivateKey.derive(44, true).derive(64, true).derive(0, true).derive(0).derive(0);
-//Wallet Address is  "/p2pkh/"+derivedKey.privateKey.toAddress().toString()+"/";
-console.log(derivedKey.privateKey.toAddress().toString());
+var derivedKey = hdPrivateKey.derive(44, true).derive(64, true).derive(0, true).derive(0).derive(0);
+console.log("Passphrase is: " + code.toString() + "\nWalletID: " + derivedKey.privateKey.toAddress().toString());
+
+}
+
+//Existing Account Query
+//Asset path needs to be in full i.e. /asset/p2pkh/XcDCGPMtdrKxodQ4soFyYfDmr78gTvJ9jN/
+function existingAccountQuery(passphrase, asset) {
+    var client = new openchain.ApiClient("http://0.0.0.0:8080/");
+
+    //Calculate Private Key and wallet path from passphrase.
+    var tmpMnenoic = new Mnemonic(passphrase);
+    var privateKey = tmpMnenoic.toHDPrivateKey();
+    var hdPrivateKey = new bitcore.HDPrivateKey(privateKey.toString());
+    hdPrivateKey.network = bitcore.Networks.get("openchain");
+    
+    var derivedKey = hdPrivateKey.derive(44, true).derive(64, true).derive(0, true).derive(0).derive(0);
+    var walletId = "/p2pkh/" + derivedKey.privateKey.toAddress().toString() + "/";
+
+    client.getAccountRecord(walletId, asset)
+    .then(function (result) {
+        console.log("Account Balance: " + result.balance.toString());
+    });
+}
+
+function testDoNotUse() {
+// var acc2 = "license saddle depart vintage nurse promote renew suggest steak already flush avocado";
+// var acc2Mne = new Mnemonic(acc2);
+// // var secondPrivateKey = acc2Mne.toHDPrivateKey();
+// // console.log(secondPrivateKey.);
+
+// var t = new Mnemonic();
+// var makeKey = acc2Mne.toHDPrivateKey() //I think u missed toHDprivatekey
+// var hdPrivateKey = new bitcore.HDPrivateKey(makeKey.toString());
+// hdPrivateKey.network = bitcore.Networks.get("openchain");
+// derivedKey = hdPrivateKey.derive(44, true).derive(64, true).derive(0, true).derive(0).derive(0);
+// //Wallet Address is  "/p2pkh/"+derivedKey.privateKey.toAddress().toString()+"/";
+// console.log(derivedKey.privateKey.toAddress().toString());
+}
+
+existingAccountQuery("deny depend label valid belt begin gesture extend list they outer glove", "/asset/p2pkh/XddsvA9fJurr4mEPggsLiM7BwkJjDtDi2o/");
